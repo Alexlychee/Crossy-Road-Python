67036ca509ce0b71238d65a3a3c8377853e712eb
Alex Ly <70249534+Alexlychee@users.noreply.github.com>
2024-10-08 21:09:39 -0700

    added cars into the game that moves to the left

--- car_manager.py
+++ car_manager.py
@@ -6,15 +6,26 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager:
     """
     Create cars that are 20px high by 40px wide that are randomly generated along the y-axis and move to the left edge of the screen.
     No cars should be generated in the top and bottom 50px of the screen (think of it as a safe zone for our little turtle).
     Hint: generate a new car only every 6th time the game loop runs. If you get stuck, check the video walkthrough in Step 4.
     """
     def __init__(self):
-        super().__init__()
-        self.shape("square")
-        self.color(random.choice(COLORS))
-        self.shapesize(stretch_wid=2, stretch_len=1)
-        self.left(90)
+        self.all_cars = []
+
+    def create_cars(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for cars in self.all_cars:
+            cars.backward(STARTING_MOVE_DISTANCE)
